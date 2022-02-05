eb4fe18dfd574e691f00a09563ab5d0a9c5def75
MoutasemMK <87209889+MoutasemMK@users.noreply.github.com>
2022-02-05 17:15:34 +0100

    Update test.js

--- test.js
+++ test.js
@@ -3,7 +3,9 @@ const tax = require("./index")
 
 console.log(tax.specific("70k","5000",true))// without true will give a erorr or type 700000
 const result = tax.tax("1.7m",true)//without true will give a erorr or type 1700000
-console.log(result)
+if(result){
+    console.log(result)
+}
 /*
 result: {
 tax: 17894737,
