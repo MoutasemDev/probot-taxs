0312d8704b1c103e8fcccd1258d9f527b14686f4
MoutasemMK <moutasemse1@gmail.com>
2022-02-05 13:34:33 +0100

    first commit

---
+++ test.js
@@ -0,0 +1,13 @@
+const tax = require("./index")
+
+
+console.log(tax.specific("70k","5000",true))// without true will give a erorr or type 700000
+const result = tax.tax("1.7m",true)//without true will give a erorr or type 1700000
+console.log(result)
+/*
+result: {
+tax: 17894737,
+wasit: 18836566,
+difference: 894737 
+}
+*/
